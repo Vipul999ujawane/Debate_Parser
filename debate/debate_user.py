7bce11b7e062e0f91be4154504ad4d16a7272fec
Vipul Ujawane <vipul999ujawane@gmail.com>
2019-08-17 03:58:02 +0530

    Added User Parser

--- debate/debate_user.py
+++ debate/debate_user.py
@@ -4,9 +4,23 @@ import requests
 
 from bs4 import BeautifulSoup
 
+def get_user(html):
+    soup = BeautifulSoup(html,"html.parser")
+    a = soup.find("a")
+    link = "http:{}".format(a['href'])
+    user = link.split('/')[-1]
+    return (user,link)
+
+def get_post(html):
+    pass
+
+def parse_arguments(html):
+    soup = BeautifulSoup(html,"html.parser")
+    arguments = soup.find_all("div",class_="argBox")
+    get_user(str(arguments[0]))
+
 def get_user_arguments(response):
     soup = BeautifulSoup(response.text,"html.parser")
-    deb_sides=soup.find_all("div",class_="debateSideBox")
-    print(deb_sides)
-
-
+    left=soup.find("div",class_="debateSideBox sideL")
+    right=soup.find("div",class_="debateSideBox sideR")
+    parse_arguments(str(left))
